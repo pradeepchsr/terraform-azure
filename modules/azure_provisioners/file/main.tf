2c239f90fc3bc15d097342154dd76dcdcb7e8e1f
Pradeep Chittoor <pradeep.chittoor@gyansys.com>
2024-02-28 17:35:02 +0530

    changes made to provider file

--- modules/azure_provisioners/file/main.tf
+++ modules/azure_provisioners/file/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.93.0"
+    }
+  }
+}
+
+
 
 resource "azurerm_resource_group" "pradeep-rg" {
   name     = var.rgname
